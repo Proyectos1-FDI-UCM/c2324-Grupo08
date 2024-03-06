b4f56fcbd124ba1f3c2b1a7ec4c49578b432c3c5
Javigozu <javigozu5127@gmail.com>
2024-03-06 12:54:44 +0100

    Arreglado error GM (Hay que descomentarlo))

--- DeliriumTest/Assets/Scripts/GameManager.cs
+++ DeliriumTest/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-
+/*
 public class GameManager : MonoBehaviour
 {
     public GameObject[] Rooms;
@@ -56,4 +56,4 @@ public class GameManager : MonoBehaviour
     {
 
     }
-}
+}*/
