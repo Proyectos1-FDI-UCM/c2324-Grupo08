ddb82c5b87f4b0b2d03e88dfc4ae12e426a0e1fa
navajaass <157645501+navajaas@users.noreply.github.com>
2024-04-24 11:17:14 +0200

    Indicador de mejoras
    Cuando el jugador obtiene una mejora sale un cartel informando de ella. Tengo que cambiar los sprites, pero funciona bien

--- DeliriumTest/Assets/Scripts/UIManager.cs
+++ DeliriumTest/Assets/Scripts/UIManager.cs
@@ -17,6 +17,15 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     Image imgEnergetica;
 
+    [SerializeField]
+    Image imgIndEng;
+    [SerializeField]
+    Image imgIndKeb;
+    [SerializeField]
+    Image imgIndChap;
+
+    public float timeMejora;
+
     static UIManager uiManager;
     public static UIManager UiManager { get { return uiManager; } }
     private void Awake()
@@ -32,6 +41,7 @@ public class UIManager : MonoBehaviour
         if (value == 0) imgBotella.enabled = true;
         else if (value == 1) imgCono.enabled = true;
 
+
     }
     public void QuitarSpriteBotella()
     {
@@ -46,8 +56,22 @@ public class UIManager : MonoBehaviour
     public void PonerMejora(int mejora)
     {
         if (mejora == 3) imgKebab.enabled = true;
-        if(mejora == 5) imgChapitas.enabled = true;
-        if(mejora == 4) imgEnergetica.enabled = true;
+
+        if (mejora == 5) imgChapitas.enabled = true;
+
+        if (mejora == 4) imgEnergetica.enabled = true;
+    }
+
+    public void PonerIndMejora(int mejora)
+    {
+        if (mejora == 3) imgIndKeb.enabled = true;
+
+        else if (mejora == 5) imgIndChap.enabled = true;
+
+        else if (mejora == 4)imgIndEng.enabled = true;
+
+        StartCoroutine(DeactivateMejora(mejora));
+        
     }
 
     private void Start()
@@ -57,5 +81,22 @@ public class UIManager : MonoBehaviour
         imgChapitas.enabled = false;
         imgKebab.enabled = false;
         imgEnergetica.enabled = false;
+        imgIndChap.enabled=false;
+        imgIndEng.enabled=false;
+        imgIndKeb.enabled=false;
     }
+
+
+    IEnumerator DeactivateMejora(int mejora)
+    {
+        yield return new WaitForSeconds(timeMejora);
+        if (mejora == 3) imgIndKeb.enabled = false;
+
+        else if (mejora == 5) imgIndChap.enabled = false;
+
+        else if (mejora == 4) imgIndEng.enabled = false;
+
+        StopCoroutine(DeactivateMejora(mejora));
+    }
+    
 }
